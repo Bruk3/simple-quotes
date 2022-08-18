a7b4a98f03890e562040b52165d069225c993ad2
Bruk Zewdie <bbz2103@columbia.edu>
2022-08-18 00:34:23 +0000

    lint

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask import jsonify
 import random
 
-QUOTES_FILE = "./quotes.txt" # quote file
-quotes = [] # stores all quotes
+QUOTES_FILE = "./quotes.txt"  # quote file
+quotes = []  # stores all quotes
 
 # a quote
 class Quote(object):
@@ -11,28 +11,32 @@ class Quote(object):
         self.quote = quote
         self.by = by
 
+
 # Loads quotes from a file
 def loadQuotes():
     with open(QUOTES_FILE) as file:
         lines = file.readlines()
-        lines = [x.strip() for x in lines] 
+        lines = [x.strip() for x in lines]
         for line in lines:
             quote, by = line.split("-")
             quotes.append(Quote(quote, by))
-            
+
+
 app = Flask(__name__)
 
-# Gets a random quote 
+# Gets a random quote
 @app.route("/api/quote")
 def quote():
-    q = random.choice(quotes) # selects a random quote from file
-    return jsonify({"quote": q.quote, "by": q.by}) # return a quote
+    q = random.choice(quotes)  # selects a random quote from file
+    return jsonify({"quote": q.quote, "by": q.by})  # return a quote
+
 
 # 404 Error for unknown routes
 @app.errorhandler(404)
 def page_not_found(e):
     return jsonify({"message": "Resource not found"}), 404
 
-if __name__ == '__main__':
-    loadQuotes() # load quotes 
-    app.run(host='0.0.0.0', port=5000, debug=True) # run application
+
+if __name__ == "__main__":
+    loadQuotes()  # load quotes
+    app.run(host="0.0.0.0", port=5000, debug=True)  # run application
